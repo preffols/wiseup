f4263093651ab07cc762f9f8a797c8deee0ce018
Prince Escales Nsusa <87782239+preffols@users.noreply.github.com>
2022-07-01 06:20:45 -0700

    conn

--- E-products/includes/common.php
+++ E-products/includes/common.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("31.220.110.201","u695029436_root","!pRINCE347","u695029436_wise_up_youth");
+$con=mysqli_connect("31.220.110.201","u695029436_root","!pRINCE1347","u695029436_wise_up_youth");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
