868b9fdac83071f08d1b6146ef0ea4a30a6d6194
Prince Escales Nsusa <87782239+preffols@users.noreply.github.com>
2022-07-07 05:41:39 -0700

    about page

--- E-products/includes/common.php
+++ E-products/includes/common.php
@@ -1,5 +1,7 @@
 <?php
+
 $con=mysqli_connect("mysql.hostinger.ph","u695029436_root","!pRINCE1347","u695029436_wise_up_youth");
+// $con=mysqli_connect("localhost","root","","e-Products");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
