1bd0f6323ee236c1a6d3d408d42e842dba89d016
Prince Escales Nsusa <87782239+preffols@users.noreply.github.com>
2022-07-12 21:27:22 -0700

    logo index footer

--- index.php
+++ index.php
@@ -757,7 +757,7 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>E-envoy</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Wise Up Youth</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         
